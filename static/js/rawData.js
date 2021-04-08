84890f780018696f8cdacff8c517be5510952125
ottovenliu <76770643+ottovenliu@users.noreply.github.com>
2021-04-09 01:23:11 +0800

    0409_final_1.0.0

--- static/js/rawData.js
+++ static/js/rawData.js
@@ -19,34 +19,3 @@ function formDatabox() {
     };
     req.send();
 }
-        // var contextArea = JSON.parse(req.responseText);
-        // console.log(contextArea);
-        // const form = document.forms['searchForm'];
-        // const name = form.elements.searchName.value;
-        // var usernamebox = []
-        // if (name.length > 0) {
-        //     for (var i = 0; i < contextArea["data"].length; i++) {
-        //         usernamebox.push(contextArea["data"][i]["username"])
-        //     }
-        //     var userindex = usernamebox.indexOf(name)
-        //     if (userindex > 0) {
-        //         var box = document.getElementById('searchresult');
-                // box.textContent = contextArea["data"][userindex]["name"] + "(" + contextArea["data"][userindex]["username"] + ")";
-    //             box.textContent = "{{userInfo_json}}";
-    //         }
-    //         else {
-    //             var box = document.getElementById('searchresult');
-    //             // box.textContent = "沒有這個使用者";
-    //             box.textContent = "{{userInfo_json}}";
-    //         }
-    //     }
-    //     else {
-    //         var box = document.getElementById('searchresult');
-    //         box.textContent = "輸入值為空,請重新輸入查詢帳號";
-    //         alert("輸入值為空,請重新輸入查詢帳號")
-    //     }
-
-
-//     };
-// req.send(null);
-// }
